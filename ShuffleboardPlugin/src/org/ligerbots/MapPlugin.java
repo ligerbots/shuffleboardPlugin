ba48854157a52bbc61bae9d144c022d1abf6724c
Matthew Leonard <matthewdleonard01@gmail.com>
2018-02-07 20:29:15 -0500

    Began laying out plugin

---
+++ ShuffleboardPlugin/src/org/ligerbots/MapPlugin.java
@@ -0,0 +1,9 @@
+package org.ligerbots;
+
+public class MapPlugin extends edu.wpi.first.shuffleboard.api.plugin.Plugin {
+
+	protected MapPlugin() {
+		super("2877 Plugins", "Map Plugin", "1.0", "This plugin does a thing");
+	}
+
+}
---
+++ ShuffleboardPlugin/src/org/ligerbots/Widgets/MapWidget.java
@@ -0,0 +1,18 @@
+package org.ligerbots.Widgets;
+
+import edu.wpi.first.shuffleboard.api.sources.DataSource;
+import edu.wpi.first.shuffleboard.api.sources.SubSource;
+import edu.wpi.first.shuffleboard.api.widget.SimpleAnnotatedWidget;
+import edu.wpi.first.shuffleboard.api.widget.Widget;
+
+import java.util.Collection;
+import java.util.function.BiFunction;
+import java.util.function.ToDoubleFunction;
+import java.util.stream.Stream;
+
+import javafx.scene.layout.Region;
+import javafx.scene.image.image;
+
+public class MapWidget extends {
+	
+}
